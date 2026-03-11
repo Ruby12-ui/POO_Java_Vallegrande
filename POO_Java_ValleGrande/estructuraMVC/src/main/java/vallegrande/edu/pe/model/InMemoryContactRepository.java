83037c9e53c2822672835a2766e47db6622276fa
Ruby12-ui <rcubas@vallegrande.edu.pe>
2026-03-11 09:06:11 -0500

    Add files via upload

---
+++ POO_Java_ValleGrande/estructuraMVC/src/main/java/vallegrande/edu/pe/model/InMemoryContactRepository.java
@@ -0,0 +1,10 @@
+package vallegrande.edu.pe.model;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Implementación en memoria del repositorio de contactos.
+ */
+public class InMemoryContactRepository implements ContactRepository {
+    private Map< String, Contact> store }
